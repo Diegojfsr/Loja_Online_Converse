517772a73406e92ace4577cb41fd19293a2e4180
Diego Jefferson <diegojfsr@gmail.com>
2023-05-01 12:58:00 -0300

    Loja Converse

---
+++ LojaOnLine Converse/LojaCalcados/pizzas.js
@@ -0,0 +1,98 @@
+let pizzaJson = [
+    {
+        id: 1,
+        name: 'Cano Alto',
+        img: '../LojaCalcados/images/AllStars/air.png',
+        price: [20.00, 23.00, 25.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Lorem ipsum dolor sit amet consectetur impal adipisicing elit.Alias assumenda dolorum doloremque sapiente aliquid aperiam.'
+      },
+      {
+        id: 2,
+        name: 'Cano Alto',
+        img: '../LojaCalcados/images/AllStars/air.png',
+        price: [21.00, 24.00, 26.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Lorem ipsum dolor sit amet consectetur impal adipisicing elit.Alias assumenda dolorum doloremque sapiente aliquid aperiam.'
+      },
+      {
+        id: 3,
+        name: 'Cano Alto',
+        img: '../LojaCalcados/images/AllStars/air.png',
+        price: [23.00, 26.00, 28.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Lorem ipsum dolor sit amet consectetur impal adipisicing elit.Alias assumenda dolorum doloremque sapiente aliquid aperiam.'
+      },
+      {
+        id: 4,
+        name: 'Cano Alto',
+        img: '../LojaCalcados/images/AllStars/air.png',
+        price: [25.00, 28.00, 30.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Lorem ipsum dolor sit amet consectetur impal adipisicing elit.Alias assumenda dolorum doloremque sapiente aliquid aperiam.'
+      },
+      {
+        id: 5,
+        name: 'Cano Alto',
+        img: '../LojaCalcados/images/AllStars/air.png',
+        price: [24.00, 27.00, 29.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Lorem ipsum dolor sit amet consectetur impal adipisicing elit.Alias assumenda dolorum doloremque sapiente aliquid aperiam.'
+      },
+      {
+        id: 6,
+        name: 'Cano Alto',
+        img: '../LojaCalcados/images/AllStars/air.png',
+        price: [30.00, 33.00, 35.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Lorem ipsum dolor sit amet consectetur impal adipisicing elit.Alias assumenda dolorum doloremque sapiente aliquid aperiam.'
+      },
+      {
+        id: 7,
+        name: 'Cano Alto',
+        img: '../LojaCalcados/images/AllStars/air.png',
+        price: [27.00, 30.00, 32.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Lorem ipsum dolor sit amet consectetur impal adipisicing elit.Alias assumenda dolorum doloremque sapiente aliquid aperiam.'
+      },
+      {
+        id: 8,
+        name: 'Cano Alto',
+        img: '../LojaCalcados/images/AllStars/air.png',
+        price: [30.00, 32.00, 35.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Lorem ipsum dolor sit amet consectetur impal adipisicing elit.Alias assumenda dolorum doloremque sapiente aliquid aperiam.'
+      }
+];
